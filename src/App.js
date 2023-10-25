03a636a835c469e575b444fd55e0efdfcf86c3e6
Lola Castiilo <larrycastillo2009@gmail.com>
2023-10-25 15:23:13 -0500

    goodbye

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from "./components/Navigation";
 import Home from './components/Home';
 import About from './components/AboutUs';
 import Blog from './components/Blog';
 import Release from './components/Releases';
+import {db} from './firebase/config';
+import {doc, getDoc} from 'firebase/firestore';
 
 function App() {
+    useEffect(function(){
+        const docRef = doc(db, "releases", "1");
+        getDoc(docRef).then(docSnap => {
+           const release = docSnap.data();
+           console.log(release);
+        });
+    }, []);
     return (
         <BrowserRouter>
             <Navigation/>
